5ea4b009a591112bfe51555cfc1f8094db3512c7
Avi <avidrucker@gmail.com>
2020-09-09 01:32:01 -0400

    feat button click submits input text, clears input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { IAppData, createBlankData } from 'fp-autofocus';
+import React, { useState } from 'react';
+import { createBlankData, SIMcreateAndAddNewItem } from 'fp-autofocus';
 import './App.css';
 
 function App() {
-	const myApp: IAppData = createBlankData();
+	const [myApp, setMyApp] = useState(createBlankData());
+	const [text, setText] = useState('');
+
+	function handleTextChange(e: any) {
+		setText(e.target.value);
+	}
+
+	const clearText =  () => {
+		setText('');
+	}
+
+	function handleSubmit(e: any) {
+		e.preventDefault();
+		setMyApp(SIMcreateAndAddNewItem(myApp)(text));
+		clearText();
+	}
 
 	const listItems =
-		<ul>{myApp.myList.map(x => <li>{x.textName}</li>)}</ul>;
+		<ul>{myApp.myList.map(x => <li key={x.id}>{x.textName}</li>)}</ul>;
 
   return (
     <section className="debug">
       <section>{listItems}</section>
 			<section>Last done is {myApp.lastDone}</section>
+			<form onSubmit={handleSubmit}>
+				<label>Enter To-Do Text:</label>
+				<input onChange={handleTextChange} value={text} />
+				<button type="submit">Submit</button>
+			</form>
     </section>
   );
 }
