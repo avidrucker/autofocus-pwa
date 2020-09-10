0e414b49f20c79c37d73268807c09812d38c1dc4
Avi <avidrucker@gmail.com>
2020-09-10 10:10:13 -0400

    feat new header mol

---
+++ src/components2/AppHeaderMol.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AppHeaderMol = () =>
+	<header className="relative w-100 pa2 bg-blue white">
+		<h1 className="ma0 pa0">AutoFocus</h1>
+	</header>
+
+export default AppHeaderMol;
