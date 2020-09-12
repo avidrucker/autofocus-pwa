fae32cc0c2f55b325d7b3c5121b43d5299fea252
Avi <avidrucker@gmail.com>
2020-09-11 23:41:40 -0400

    feat nav link icons, update to-do comments

--- src/components2/AppHeaderMol.tsx
+++ src/components2/AppHeaderMol.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import EmojiAtom from './EmojiAtom';
 
+// TODO: add drop down menu which uses
+//   emoji enhanced options such as
+//   toggle show completed <EmojiAtom symbol="👁️" />
+
+// more useful emoji: <EmojiAtom symbol="👂" />
+// information: <EmojiAtom symbol="ℹ️" />
+// accessibility: ♿
+// gear / settings: ⚙️
+// hamburgur icon: ☰
+
+// TODO: consider how to make this app entirely
+//    accessibile to the visually impaired
+// TODO: consider how to make this app entirely
+//    accessibile to the physically impaired
 const AppHeaderMol = () =>
-	<header className="relative w-100 pa2 bg-blue white">
-		<h1 className="ma0 pa0">AutoFocus</h1>
+	<header className="w-100 pa3 bg-blue white flex items-center justify-between">
+		<span className="pa2 ph3 f3">
+			<EmojiAtom symbol="ℹ️" label="about" />
+			{/* <span className="f6"> About</span> */}
+		</span>
+		<h1 className="ma0 pv0 ph2">AutoFocus</h1>
+		<span className="b pa2 ph3 f3">
+			<EmojiAtom symbol="☰" label="menu" />
+			{/*<span className="f6"> Menu</span>*/}
+		</span>
 	</header>
 
 export default AppHeaderMol;
