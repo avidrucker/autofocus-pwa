e42a151795345a0e4d0217d75693452aee9cd70e
Avi <avidrucker@gmail.com>
2020-09-11 23:40:49 -0400

    feat last done indicator component

---
+++ src/components2/LastDoneAtom.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+const LastDoneAtom = (props: any) =>
+	<section className="ph3">
+		<p className="tl">{props.lastDoneText}</p>
+	</section>
+
+export default LastDoneAtom;
