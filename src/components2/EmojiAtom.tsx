1413321f7b058387e3adc63b214451a80faf8ab2
Avi <avidrucker@gmail.com>
2020-09-11 23:41:06 -0400

    update include emoji atom

---
+++ src/components2/EmojiAtom.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// source: https://medium.com/@seanmcp/%EF%B8%8F-how-to-use-emojis-in-react-d23bbf608bf7
+const EmojiAtom = (props: any) => (
+    <span
+        className="emoji"
+        role="img"
+        aria-label={props.label ? props.label : ""}
+        aria-hidden={props.label ? "false" : "true"}
+    >
+        {props.symbol}
+    </span>
+);
+
+export default EmojiAtom;
